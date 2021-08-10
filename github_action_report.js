e6c447aadfc86e8da016add60343e527631f7f09
GitHub Action <action@github.com>
2021-08-10 05:25:22 +0000

    Add generated data

--- github_action_report.js
+++ github_action_report.js
@@ -54,8 +54,8 @@ google.charts.load("current", { packages: ["corechart"] });
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
         ['Job Status', 'Total'],
-        ['Success', 3933],
-        ['Failure', 74]
+        ['Success', 3980],
+        ['Failure', 69]
     ]);
     var options = {
         title: 'Runs Overview for the last 5 runs',
@@ -70,8 +70,8 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart_last_run() {
     var data = google.visualization.arrayToDataTable([
         ['Job Status', 'Total'],
-        ['Success', 796],
-        ['Failure', 15]
+        ['Success', 807],
+        ['Failure', 9]
     ]);
     var options = {
         title: 'Runs Overview for last night run',
