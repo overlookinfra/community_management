ed5ca1aa3cdf939c08fb32d11dfb99685ab83937
daiana <daiana.mezdrea@yahoo.com>
2021-09-28 16:02:24 +0300

    Add failed steps chart for github action report

--- github_actions_report.rb
+++ github_actions_report.rb
@@ -16,11 +16,14 @@ count = 0
 def parse_job(job,module_name)
   @general_conclusion = true if job['conclusion'] == 'failure'
   matches = job['name'].split(',')
+  step_failure = job['steps'].select { |step| step['conclusion'] == 'failure' }.map {|step| step['name']}.first
+  step_failure = 'nil' if step_failure.nil?
   {
     "os": matches[0],
     "agent": matches[1][1..],
     "result": job['conclusion'],
-    "url": "https://github.com/#{module_name}/runs/#{job['id']}?check_suite_focus=true" 
+    "url": "https://github.com/#{module_name}/runs/#{job['id']}?check_suite_focus=true",
+    "step_failure": step_failure
   }
 end
 parsed.each do |_k, v|
@@ -43,6 +46,12 @@ parsed.each do |_k, v|
       job_failures = os_agent.select { |x| x[:result] == 'failure' }.length
       job_successes = os_agent.select { |x| x[:result] == 'success' }.length
       util.check_limit_api()
+      step_failures = { 
+        'provision': os_agent.select { |x| x[:step_failure].downcase == 'provision test environment'}.length,
+        'acceptance': os_agent.select { |x| x[:step_failure].downcase == 'run acceptance tests'}.length,
+        'agent': os_agent.select { |x| x[:step_failure].downcase == 'install agent'}.length,
+        'module': os_agent.select { |x| x[:step_failure].downcase == 'install module'}.length
+      }
       runs_array << {
         "run_id": run['id'],
         "run_number": run['run_number'],
@@ -52,7 +61,8 @@ parsed.each do |_k, v|
         "os_agent": os_agent.sort_by { |hash| hash[:agent] },
         "general_conclusion": @general_conclusion,
         "job_failures": job_failures,
-        "job_successes": job_successes
+        "job_successes": job_successes,
+        "step_failure": step_failures
       }
     end
 
@@ -64,7 +74,15 @@ parsed.each do |_k, v|
       "total_failures": runs_array.map { |x| x[:job_failures]}.reduce(0) { |sum, num| sum + num },
       "total_successes": runs_array.map { |x| x[:job_successes]}.reduce(0) { |sum, num| sum + num },
       "last_night_failures": runs_array[0][:job_failures], 
-      "last_night_successes": runs_array[0][:job_successes]
+      "last_night_successes": runs_array[0][:job_successes],
+      "last_night_provision_failures": runs_array[0][:step_failure][:provision],
+      "last_night_install_agent_failures": runs_array[0][:step_failure][:agent],
+      "last_night_install_module_failures": runs_array[0][:step_failure][:module],
+      "last_night_run_acceptance_failures": runs_array[0][:step_failure][:acceptance],
+      "total_install_agent_failures": runs_array.map { |x| x[:step_failure][:agent]}.reduce(0) { |sum, num| sum + num },
+      "total_install_module_failures": runs_array.map { |x| x[:step_failure][:module]}.reduce(0) { |sum, num| sum+ num },
+      "total_provision_failures": runs_array.map { |x| x[:step_failure][:provision]}.reduce(0) { |sum, num| sum+ num },
+      "total_run_acceptance_failures": runs_array.map { |x| x[:step_failure][:acceptance]}.reduce(0) { |sum, num| sum + num }
     }
   end
 rescue StandardError => e
@@ -84,7 +102,11 @@ html = ERB.new(File.read('github_actions_report.html.erb')).result(binding)
 File.open('GithubActionsReport.html', 'wb') do |f|
   f.puts(html)
 end
-js = ERB.new(File.read('github_action_report.js.erb')).result(binding)
-File.open('github_action_report.js', 'wb') do |f|
+js = ERB.new(File.read('charts.js.erb')).result(binding)
+File.open('charts.js', 'wb') do |f|
   f.puts(js)
+end
+dashboard = ERB.new(File.read('dashboard.html.erb')).result(binding)
+File.open('dashboard.html', 'wb') do |f|
+  f.puts(dashboard)
 end
